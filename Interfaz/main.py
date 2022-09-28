f09da2d94738789d5236b877f251d9f4e4c84c29
sgomezh <tifa.peace@gmail.com>
2022-09-28 18:24:20 -0300

    Interfaz gráfica sin funcionalidades

--- Interfaz/main.py
+++ Interfaz/main.py
@@ -43,25 +43,25 @@ dowhy_label = tk.Label(main_page, text="DoWhy", font=label_font, bg='#08013D', f
 dowhy_label.place(x=anchura-180, y=50)
 #buttons BART
 button2 = tk.Button(main_page, text="Predict", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
-button2.place(x=10, y=130)
+button2.place(x=20, y=130)
 
 button3 = tk.Button(main_page, text="RMSE", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
-button3.place(x=10, y=210)
+button3.place(x=20, y=210)
 
 button4 = tk.Button(main_page, text="Variable \nImportance", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
-button4.place(x=10, y=290)
+button4.place(x=20, y=290)
 '''
 button5 = tk.Button(main_page, text="Click Me!", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
-button5.place(x=10, y=370)
+button5.place(x=20, y=370)
 
 button6 = tk.Button(main_page, text="Click Me!", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
-button6.place(x=10, y=450)
+button6.place(x=20, y=450)
 
 button7 = tk.Button(main_page, text="Click Me!", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
-button7.place(x=10, y=530)
+button7.place(x=20, y=530)
 
 button8 = tk.Button(main_page, text="Click Me!", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
-button8.place(x=10, y=610)'''
+button8.place(x=20, y=610)'''
 
 #buttons DoWhy
 button2 = tk.Button(main_page, text="Get ATE", bg="#B7B5C8", fg="black", font=button_font, width=20, height=3)
